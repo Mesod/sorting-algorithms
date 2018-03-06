8ccc5e80f17eb636ab7de5db4f1b20ed0ff54d89
Masood Adeli <masood.adeli@reyhoon.com>
2018-03-06 15:48:30 +0330

    Generating random number and saving them in files. Write functions for reading the files and benchmarking the algorithms. Shell sort is not implemented yet

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 //@ToDo: injecting all the routes, methods for generating mock data, algorithm's tester
-const bubbleSort    = require('algorithms/bubble-sort')
+const bubbleSort = require('algorithms/bubble-sort')
 const insertionSort = require('algorithms/insertion-sort')
 const selectionSort = require('algorithms/selection-sort')
-const shakerSort    = require('algorithms/shaker-sort')
-const countingSort  = require('algorithms/counting-sort')
+const shakerSort = require('algorithms/shaker-sort')
+const countingSort = require('algorithms/counting-sort')
 
-let unsortedNumbers = [6, 5, 3, 1, 8, 7, 1, 2, 4]
+const numbersArray = [10, 100, 1000, 100000, 1000000]
+const fs = require('fs')
+for (numbersCounta of numbersArray) {
+    const unsortedNumbers = JSON.parse(fs.readFileSync(`${numbersCounta}_numbers.json`))
 
-let sortedNumbersByBubble = bubbleSort(unsortedNumbers)
-
-let sortedNumbersByInsertion = insertionSort(unsortedNumbers)
-
-let sortedNumbersBySelection = selectionSort(unsortedNumbers)
-
-let sortedNumbersByShaker = shakerSort(unsortedNumbers)
-
-let sortedNumbersByCounting = countingSort(unsortedNumbers)
-
-console.log(`UNSORTED  >>>>>> ${unsortedNumbers}`)
-console.log(`BUBBLE    >>>>>> ${sortedNumbersByBubble}`)
-console.log(`INSERTION >>>>>> ${sortedNumbersByInsertion}`)
-console.log(`SELECTION >>>>>> ${sortedNumbersBySelection}`)
-console.log(`SHAKER    >>>>>> ${sortedNumbersByShaker}`)
-console.log(`COUNTING  >>>>>> ${sortedNumbersByCounting}`)
+    let date = Date.now()
+    let sortedNumbersByBubble = bubbleSort(unsortedNumbers)
+    let bubbleTime = Date.now() - date
+    
+    date = Date.now()
+    let sortedNumbersByInsertion = insertionSort(unsortedNumbers)
+    let insertionTime = Date.now() - date
+    
+    date = Date.now()
+    let sortedNumbersBySelection = selectionSort(unsortedNumbers)
+    let selectionTime = Date.now() - date
+    
+    date = Date.now()
+    let sortedNumbersByShaker = shakerSort(unsortedNumbers)
+    let shakerTime = Date.now() - date
+    
+    date = Date.now()
+    let sortedNumbersByCounting = countingSort(unsortedNumbers)
+    let coutingTime = Date.now() - date
+    
+    console.log(`sorting ${numbersCounta} numbers`)
+    console.log(`BUBBLE    >>>>>> ${bubbleTime}`)
+    console.log(`INSERTION >>>>>> ${insertionTime}`)
+    console.log(`SELECTION >>>>>> ${selectionTime}`)
+    console.log(`SHAKER    >>>>>> ${shakerTime}`)
+    console.log(`COUNTING  >>>>>> ${coutingTime}`)
+    
+}
